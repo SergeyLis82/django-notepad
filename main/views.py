518266c6890ebe846f2a9228eda4fe9fb3d8dc7d
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2021-07-26 23:26:51 +0300

    Add changes to templates

--- main/views.py
+++ main/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'main/index.html')
+  data = {
+    "title": "Main page",
+    "values": ["Some", "Hello", "2021"],
+    "obj": {"Car": "Schevrolet", "Hobby": "Sport", "Job": "DevOps"},
+  }
+  return render(request, 'main/index.html', data)
 
 def about(request):
   return render(request, 'main/about.html')
