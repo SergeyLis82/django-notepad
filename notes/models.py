dfdc7ffc1cd67e307c8a3b4c9a1325359760c9e0
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2021-07-27 21:03:45 +0300

    Add new classes for db-tables

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField('Заголовок', max_length=100)
+    preview = models.CharField('Превью', max_length=250)
+    full_text = models.TextField('Текст')
+    date = models.DateTimeField('Дата создания')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
--- notes/views.py
+++ notes/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def notes(request):
-    return render(request, 'main/about.html')
+    return render(request, 'notes/notes.html')
