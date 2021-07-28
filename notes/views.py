b02ca472d35ab1d8c11ef77cbeefdb79f350fd93
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2021-07-28 10:05:12 +0300

    Update form notes

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
 
 def notes(request):
-    return render(request, 'notes/notes.html')
+    notes = Notes.objects.order_by('-date')
+    return render(request, 'notes/notes.html', {'notes': notes})
