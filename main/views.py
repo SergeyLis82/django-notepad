ebd254c4a287ce75b8e75f240660d17a5a83dc4d
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2021-11-29 15:08:41 +0300

    pages update

--- main/views.py
+++ main/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 
 def index(request):
   data = {
-    "title": "Привет!",
-    "says": "Никто не стремится получать советы, зато деньги получать горазды все;\nвыходит - деньги лучше, чем советы.\n		-- Дж.Свифт"
+    "title": "Home",
+    "heading": "Привет!",
+    "content": "Никто не стремится получать советы, зато деньги получать горазды все;\nвыходит - деньги лучше, чем советы.\n		-- Дж.Свифт"
   }
   return render(request, 'main/index.html', data)
 
 def about(request):
-  return render(request, 'main/about.html')
+  data = {
+    "title": "About",
+    "heading": "About page",
+    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sit voluptatum, praesentium, corrupti illo aliquam blanditiis officiis incidunt obcaecati nisi assumenda. Praesentium doloribus eos commodi reiciendis culpa animi ut et"
+  }
+  return render(request, 'main/about.html', data)
