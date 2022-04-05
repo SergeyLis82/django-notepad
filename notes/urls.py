056dc9558ea3a204f4c14d47301c475dc950ff42
Лысенко Сергей Викторович <lysenko.sergey@zolotoy.ru>
2022-04-05 22:49:12 +0300

    Add edit and delete groups

--- notes/urls.py
+++ notes/urls.py
@@ -5,11 +5,13 @@ app_name = 'notes'
 
 urlpatterns = [
     path('', views_notes.view_notes, name='view_notes'),
-    path('', views_groups.view_groups, name='view_groups'),
     path('add_note/', views_notes.add_note, name='add_note'),
     path('<int:note_id>/', views_notes.detail, name='detail'),
     path('<int:note_id>/edit/', views_notes.edit_note, name='edit_note'),
     path('<int:note_id>/delete/', views_notes.delete_note, name='delete_note'),
     path('groups/', views_groups.view_groups, name='view_groups'),
     path('groups/<int:group_id>/', views_groups.detail, name='group_detail'),
+    path('groups/add_group/', views_groups.add_group, name='add_group'),
+    path('groups/<int:group_id>/edit/', views_groups.edit_group, name='edit_group'),
+    path('groups/<int:group_id>/delete/', views_groups.delete_group, name='delete_group'),
 ]
--- notes/views/views_groups.py
+++ notes/views/views_groups.py
@@ -20,3 +20,50 @@ def detail(request, group_id):
     'group': group,
     }
     return render(request, 'notes/group.html', data)
+
+def add_group(request):
+    error = ''
+    if request.method == 'POST':
+        form = NotesGroupsForms(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes:view_groups')
+        else:
+            error = "Форма заполнена неверно"
+
+    form = NotesGroupsForms()
+
+    data = {"title": "New note",
+            "heading": "Добавление новой группы",
+            'form': form,
+            'error': error}
+
+    return render(request, 'notes/group_edit.html', data)
+
+def edit_group(request, group_id):
+    group = get_object_or_404(NotesGroups, pk=group_id)
+    error = ''
+    # Обработка метода GET
+    if request.method == 'GET':
+        form = NotesGroupsForms(instance=group)
+        data = {"title": "Update note",
+                "heading": f"Изменение группы - {group.groupname}",
+                'group': group,
+                'form': form,
+                'error': error
+                }
+        return render(request, 'notes/group_edit.html', data)
+    # Обработка метода POST
+    else:
+        form = NotesGroupsForms(request.POST, instance=group)
+        if form.is_valid():
+            group.date_update = timezone.now()
+            form.save()
+            return redirect('notes:view_groups')
+        else:
+            error = "Форма заполнена неверно"
+
+def delete_group(request, group_id):
+    group = get_object_or_404(NotesGroups, pk=group_id)
+    group.delete()
+    return redirect('notes:view_groups')
